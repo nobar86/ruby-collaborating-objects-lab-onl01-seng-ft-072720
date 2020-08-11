986169bc9c3eb6540dfe160e152124a84dca0f8b
Michael Rabon <nobar86@gmail.com>
2020-08-11 17:27:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,8 @@ class Song
     @@all << self
   end
   
+  
+  
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
@@ -19,8 +21,5 @@ class Song
     song
   end
   
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+  
 end
