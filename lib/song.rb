291f8fdbb3084c5155814648a1267eff2279624c
Michael Rabon <nobar86@gmail.com>
2020-08-11 17:28:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,11 @@ class Song
     @@all << self
   end
   
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+  
   
   
   def self.new_by_filename(file)
